1c245962a6f2aaecf41f9cb5e1af3f56ff7af459
Fernando Erazo <nandocodes@NandoCODES.attlocal.net>
2023-09-30 19:27:14 -0500

    added dark/lightmode

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
+const hamburger = document.querySelector(".menu");
+const navList = document.querySelector(".nav-list");
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
+const lightmode = document.querySelector("#lightmode") ;
+const darkmode = document.querySelector("#darkmode") ;
+let clicked = false;
+//makes hamburger functional
+hamburger.addEventListener("click", () => {
+  if (!clicked) {
+    navList.style.display = "block";
 
-    const hamburger = document.querySelector(".menu");
-    const navList = document.querySelector(".nav-list");
-    
-    
+    clicked = true;
+  } else {
+    navList.style.display = "none";
 
-    let clicked =  false;
+    clicked = false;
+  }
+});
+//checks window size 
+function checkWindowSize() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth > 1000) {
+    hamburger.style.display = "none";
+    navList.style.display = "block";
+  } else {
+    hamburger.style.display = "block";
+    navList.style.display = "none";
+  }
+}
 
-    hamburger.addEventListener("click",()=>{
+checkWindowSize();
 
-        if (!clicked){
-        navList.style.display="block" ;
-        clicked=true;
-        }
-        else{
-            navList.style.display="none" ;
-            clicked=false ;
+window.addEventListener("resize", checkWindowSize);
 
-        }
-        
-        
-    })
-    
+// DARK MODE / LightMode 
+lightmode.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "#fff" ;
+    lightmode.style.display="none" ;
+    darkmode.style.display="inline-block";
 
+})
 
-    function checkWindowSize(){
-        const screenWidth= window.innerWidth ;
-        if (screenWidth > 1000){
-            hamburger.style.display="none"
-            navList.style.display="block"
-        }
-        else{
-            hamburger.style.display="block"
-            navList.style.display="none"
-            
-        }
-        
-    }
+darkmode.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "#253439" ;
+    lightmode.style.display="inline-block" ;
+    darkmode.style.display="none";
 
-checkWindowSize()
-
-window.addEventListener("resize",checkWindowSize)
+})
